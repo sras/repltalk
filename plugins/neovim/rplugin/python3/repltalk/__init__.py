a3cc8b18366581359f673a781c0f5fbacca2b502
Sandeep.C.R <sandeepcr2@gmail.com>
2019-06-12 18:37:45 +0530

    Deprecate editor adaptors in favour of async plugins

---
+++ plugins/neovim/rplugin/python3/repltalk/__init__.py
@@ -0,0 +1,78 @@
+import neovim
+import json
+import http.client
+import time
+
+def build_error_list(items, file_map=None):
+    ret = []
+    for (idx, e) in enumerate(items['errors']):
+        for (idx1, ln) in enumerate(e['text'].split('\n')):
+            # We attach each line to the error, keeping the error no (nr) same for the
+            # lines belonging to same error.
+            if idx1 == 0:
+                ret.append({'filename': apply_file_map(e['file_name'], file_map), 'lnum': e['line'], 'col': e['column'], 'text': ln, 'nr': idx, 'type': 'E'})
+            else:
+                ret.append({'text': ln, 'nr': idx, 'type': 'E'})
+
+    for (idx, e) in enumerate(items['warnings']):
+        for (idx1, ln) in enumerate(e['text'].split('\n')):
+            if idx1 == 0:
+                ret.append({'filename': apply_file_map(e['file_name'], file_map), 'lnum': e['line'], 'col': e['column'], 'text': ln, 'nr': idx, 'type': 'W'})
+            else:
+                ret.append({'text': ln, 'nr': idx, 'type': 'W'})
+    return ret
+
+def apply_file_map(path, file_map):
+    if file_map is not None:
+        for fromp in file_map:
+            if path.startswith(fromp):
+                return "{}{}".format(file_map[fromp], path[len(fromp):])
+        return path
+    else:
+        return path
+
+@neovim.plugin
+class ReplTalk(object):
+    def __init__(self, vim):
+        self.vim = vim
+
+    def repl_command(self, command, port):
+        return self.send_req(port, "/command", {'command': command})
+
+    def send_req(self, port, path, data = None):
+        while True:
+            try:
+                conn = http.client.HTTPConnection("localhost:{}".format(port))
+                if data:
+                    conn.request("POST", path, json.dumps(data), headers = {'Content-type': 'application/json'})
+                else:
+                    conn.request("GET", path)
+                break
+            except:
+                print("Connection error, retrying after 1 sec")
+                time.sleep(1)
+                pass
+        res = conn.getresponse()
+        return json.loads(res.read())
+
+    def process_output(self, msg):
+        if 'error' in msg:
+            print(msg['error'])
+        else:
+            if len(msg['output']['errors']) > 0:
+                self.vim.command('REPLTalkIndicateError')
+            elif len(msg['output']['warnings']) > 0:
+                self.vim.command('REPLTalkIndicateWarnings')
+            else:
+                self.vim.command('REPLTalkIndicateSuccess')
+            elist = build_error_list(msg['output'])
+            self.vim.funcs.setqflist([], 'r', {"items": elist, "title": "REPLTalk Error list"})
+
+    @neovim.function('REPLTalkCommand', sync=False)
+    def command_handler(self, args):
+        r = self.repl_command(args[0], args[1])
+        if 'error' in r:
+            if r['error'] == 'NOT_STARTED':
+                self.process_output(self.send_req(args[1], '/start'))
+        else:
+            self.process_output(r)
--- plugins/vim/plugin/repltalk.vim
+++ plugins/vim/plugin/repltalk.vim
@@ -1,29 +1,62 @@
-function! REPLTalkCommand(command, port)
-try
-
-python3 << en
-import vim
-import json
-import http.client
-port = vim.eval("a:port")
-data = {'command': vim.eval("a:command")}
-conn = http.client.HTTPConnection("localhost:{}".format(port))
-conn.request("POST", '/command', json.dumps(data), headers = {'Content-type': 'application/json'})
-en
-catch
-
+function! ProcessResponse(channel)
+  let full_msg = ""
+  while ch_status(a:channel, {'part': 'out'}) == 'buffered'
+    let full_msg = full_msg . ch_read(a:channel)
+  endwhile
 python << en
-import vim
 import json
-import httplib
+import vim
 
-port = vim.eval("a:port")
-data = {'command': vim.eval("a:command")}
-conn = httplib.HTTPConnection("localhost:{}".format(port))
-conn.request("POST", '/command', json.dumps(data), headers = {'Content-type': 'application/json'})
+def parse_result(resp):
+  return json.loads(resp)
+
+def build_error_list(items, file_map=None):
+    ret = []
+    for (idx, e) in enumerate(items['errors']):
+        for (idx1, ln) in enumerate(e['text'].split('\n')):
+            # We attach each line to the error, keeping the error no (nr) same for the
+            # lines belonging to same error.
+            if idx1 == 0:
+                ret.append({'filename': apply_file_map(e['file_name'], file_map), 'lnum': e['line'], 'col': e['column'], 'text': ln, 'nr': idx, 'type': 'E'})
+            else:
+                ret.append({'text': ln, 'nr': idx, 'type': 'E'})
+
+    for (idx, e) in enumerate(items['warnings']):
+        for (idx1, ln) in enumerate(e['text'].split('\n')):
+            if idx1 == 0:
+                ret.append({'filename': apply_file_map(e['file_name'], file_map), 'lnum': e['line'], 'col': e['column'], 'text': ln, 'nr': idx, 'type': 'W'})
+            else:
+                ret.append({'text': ln, 'nr': idx, 'type': 'W'})
+    return ret
+
+def apply_file_map(path, file_map):
+    if file_map is not None:
+        for fromp in file_map:
+            if path.startswith(fromp):
+                return "{}{}".format(file_map[fromp], path[len(fromp):])
+        return path
+    else:
+        return path
+
+msg = parse_result(vim.eval("full_msg"))
+
+if len(msg['output']['errors']) > 0:
+    vim.command('REPLTalkIndicateError')
+elif len(msg['output']['warnings']) > 0:
+    vim.command('REPLTalkIndicateWarnings')
+else:
+    vim.command('REPLTalkIndicateSuccess')
+
+elist = build_error_list(msg['output'])
+setfqlist = vim.Function('setqflist')
+setfqlist([], 'r', {"items": elist, "title": "REPLTalk Error list"}  )
 en
-endtry
 endfunction
+
+function REPLTalkCommand(command, port)
+  let js = ["curl", "--header", "Content-Type: application/json", "--request", "POST","--data", json_encode({"command":a:command}),  "http://localhost:".string(a:port)."/command"]
+  call job_start(js, {'close_cb': 'ProcessResponse'})
+endfunc
 
 function! REPLTalkIndicateError()
   hi StatusLine ctermfg=black guibg=black ctermbg=DarkRed guifg=#fc4242
