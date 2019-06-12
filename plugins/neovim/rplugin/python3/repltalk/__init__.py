cbde3bbf2bd7ee81e4df3b5fc7ebe18268ac0ddf
Sandeep.C.R <sandeepcr2@gmail.com>
2019-06-12 07:49:41 +0530

    Updates

--- plugins/neovim/rplugin/python3/repltalk/__init__.py
+++ plugins/neovim/rplugin/python3/repltalk/__init__.py
@@ -1,10 +1,70 @@
 import neovim
+import http.client
+
+def build_error_list(items, file_map=None):
+    ret = []
+    for (idx, e) in enumerate(items['errors']):
+        for (idx1, ln) in enumerate(e['text'].split('\n')):
+            # We attach each line to the error, keeping the error no (nr) same for the
+            # lines belonging to same error.
+            if idx1 == 0:
+                ret.append({'filename': apply_file_map(e['file_name'], file_map), 'lnum': e['line'], 'col': e['column'], 'text': ln, 'nr': idx, 'type': 'E'})
+            else:
+                ret.append({'text': ln, 'nr': idx, 'type': 'E'})
+
+    for (idx, e) in enumerate(items['warnings']):
+        for (idx1, ln) in enumerate(e['text'].split('\n')):
+            if idx1 == 0:
+                ret.append({'filename': apply_file_map(e['file_name'], file_map), 'lnum': e['line'], 'col': e['column'], 'text': ln, 'nr': idx, 'type': 'W'})
+            else:
+                ret.append({'text': ln, 'nr': idx, 'type': 'W'})
+    return ret
+
+def apply_file_map(path, file_map):
+    if file_map is not None:
+        for fromp in file_map:
+            if path.startswith(fromp):
+                return "{}{}".format(file_map[fromp], path[len(fromp):])
+        return path
+    else:
+        return path
 
 @neovim.plugin
-class Limit(object):
+class ReplTalk(object):
     def __init__(self, vim):
         self.vim = vim
-        self.calls = 0
+
+    def repl_command(self, command):
+        return self.send_req("/command", {'command': command})
+
+    def send_req(self, path, data = None):
+        while True:
+            try:
+                conn = http.client.HTTPConnection("localhost:{}".format(self.repl_port))
+                if data:
+                    conn.request("POST", path, json.dumps(data), headers = {'Content-type': 'application/json'})
+                else:
+                    conn.request("GET", path)
+                break
+            except:
+                print("Connection error, retrying after 1 sec")
+                time.sleep(1)
+                pass
+        res = conn.getresponse()
+        return json.loads(res.read())
+
+    def process_output(msg):
+        if 'error' in msg:
+            print(msg['error'])
+        else:
+            if len(msg['output']['errors']) > 0:
+                self.vim.command('REPLTalkIndicateError')
+            elif len(msg['output']['warnings']) > 0:
+                self.vim.command('REPLTalkIndicateWarnings')
+            else:
+                self.vim.command('REPLTalkIndicateSuccess')
+            elist = build_error_list(msg['output'])
+            self.vim.funcs.setfqlist([], 'r', {"items": elist, "title": "REPLTalk Error list"})
 
     @neovim.command('Cmd', range='', nargs='*', sync=True)
     def command_handler(self, args, range):
@@ -14,10 +74,17 @@ class Limit(object):
                                                                args,
                                                                range))
 
-    @neovim.autocmd('BufWritePost', pattern='*', sync=True)
+    @neovim.autocmd('BufWritePost', pattern='*', sync=False)
     def autocmd_handler(self):
-        self.vim.command("echo 123")
-        self.vim.funcs.setqflist([], 'r')
+        r = self.repl_command(":reload")
+        if 'error' in r:
+            if r.error == 'NOT_STARTED':
+                self.process_output(self.send_req('/start'))
+        else:
+            self.process_output(r)
+
+        #self.vim.command("echo 123")
+        #self.vim.funcs.setqflist([], 'r')
 
     @neovim.function('Func')
     def function_handler(self, args):
