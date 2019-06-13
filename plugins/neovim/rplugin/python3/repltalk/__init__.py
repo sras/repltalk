ea953b8cd29eecb88b84026744b7211ffcb6b46d
Sandeep.C.R <sandeepcr2@gmail.com>
2019-06-13 07:55:44 +0530

    Updates

--- plugins/neovim/rplugin/python3/repltalk/__init__.py
+++ plugins/neovim/rplugin/python3/repltalk/__init__.py
@@ -65,10 +65,7 @@ class ReplTalk(object):
                 self.vim.command('REPLTalkIndicateWarnings')
             else:
                 self.vim.command('REPLTalkIndicateSuccess')
-            try:
-                file_map = self.vim.eval('g:REPLTALK_FILE_MAP')
-            except:
-                file_map = None
+            file_map = self.vim.eval('g:REPLTALK_FILE_MAP')
             elist = build_error_list(msg['output'], file_map=file_map)
             self.vim.funcs.setqflist([], 'r', {"items": elist, "title": "REPLTalk Error list"})
 
